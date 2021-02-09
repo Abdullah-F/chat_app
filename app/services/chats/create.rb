09ee67e432314faaaa5bcc830c47458f298cbf95
Abdullah Fadhel <abdullahfadhel@outlook.com>
2021-02-09 07:55:09 +0200

    fix: remove duplication

--- app/services/chats/create.rb
+++ app/services/chats/create.rb
@@ -1,7 +1,7 @@
 module Chats
   class Create < BaseService
     def execute
-      order = fetch_order_from_redis
+      order = fetch_order
       create_chat_async(order)
       success(order: order)
     rescue ActiveRecord::RecordNotFound => e
@@ -10,14 +10,33 @@ module Chats
 
     private
 
-    def fetch_order_from_redis
-      return redis_client.incr(redis_key) if redis_client.exists?(redis_key)
-      subject = Subject.find_by!(token: token)
-      subject.transaction do
-        return redis_client.incr(redis_key) if redis_client.exists?(redis_key)
-        redis_client.set(redis_key, subject.chats_count)
-        redis_client.incr(redis_key)
+    def fetch_order
+      ActiveRecord::Base.transaction do
+        order = fetch_order_from_redis_if_exists
+        return order if order.present?
+        order = fetch_order_from_sidekiq_pending_jobs
+        order = fetch_order_from_db if order.nil?
+        set_order_on_redis(order)
       end
+    end
+
+    def fetch_order_from_redis_if_exists
+      redis_client.incr(redis_key) if redis_client.exists?(redis_key)
+    end
+
+    def fetch_order_from_sidekiq_pending_jobs
+
+    end
+
+    def fetch_order_from_db
+      subject = Subject.find_by!(token: token)
+      subject.lock!
+      subject.chats_count
+    end
+
+    def set_order_on_redis(current_order)
+      redis_client.set(redis_key, current_order)
+      redis_client.incr(redis_key)
     end
 
     def redis_client
--- app/services/messages/create.rb
+++ app/services/messages/create.rb
@@ -1,7 +1,7 @@
 module Messages
   class Create < BaseService
     def execute
-      order = fetch_order_from_redis
+      order = fetch_order
       create_message_async(order)
       success(order: order)
     rescue ActiveRecord::RecordNotFound => e
@@ -10,14 +10,33 @@ module Messages
 
     private
 
-    def fetch_order_from_redis
-      return redis_client.incr(redis_key) if redis_client.exists?(redis_key)
-      chat = Chat.find_by!(subject_token: token, order: chat_order)
-      chat.transaction do
-        return redis_client.incr(redis_key) if redis_client.exists?(redis_key)
-        redis_client.set(redis_key, chat.messages_count)
-        redis_client.incr(redis_key)
+    def fetch_order
+      ActiveRecord::Base.transaction do
+        order = fetch_order_from_redis_if_exists
+        return order if order.present?
+        order = fetch_order_from_sidekiq_pending_jobs
+        order = fetch_order_from_db if order.nil?
+        set_order_on_redis(order)
       end
+    end
+
+    def fetch_order_from_redis_if_exists
+      redis_client.incr(redis_key) if redis_client.exists?(redis_key)
+    end
+
+    def fetch_order_from_sidekiq_pending_jobs
+
+    end
+
+    def fetch_order_from_db
+      chat = Chat.find_by!(subject_token: token, order: chat_order)
+      chat.lock!
+      chat.messages_count
+    end
+
+    def set_order_on_redis(current_order)
+      redis_client.set(redis_key, current_order)
+      redis_client.incr(redis_key)
     end
 
     def redis_client
